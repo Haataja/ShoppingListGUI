30412a9d4c0678de265d7584fbd362409c9a95a4
Haataja <hanna.haataja@cs.tamk.fi>
2018-11-09 15:40:23 +0200

    initial commit

---
+++ src/main/java/Item.java
@@ -0,0 +1,45 @@
+public class Item {
+    private int quantity;
+    private String name;
+
+    public Item(String name, int quantity){
+        setName(name);
+        setQuantity(quantity);
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        if(quantity > 0){
+            this.quantity = quantity;
+        } else {
+            throw new RuntimeException("Invalid input");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void remove(int number){
+        if(quantity > number){
+            quantity -= number;
+        } else {
+            quantity = 0;
+        }
+    }
+
+    public void add(int number){
+        quantity += number;
+    }
+
+    public void add(){
+        quantity++;
+    }
+}
